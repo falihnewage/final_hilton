15d907849819f28c7b166ff6e3fc5fa8f55e86a2
falihnewage <falih@newagesmb.com>
2023-03-09 05:24:31 +0530

    new change

--- src/Axios.js
+++ src/Axios.js
@@ -4,6 +4,8 @@ const instance = axios.create({
   
   withCredentials: true,
   credentials:'include',
+  headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
   baseURL: process.env.REACT_APP_FINAL_API_URL,
+  
 });
 export default instance;
