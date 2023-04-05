25928db49c1c4f52c6ae77f09d8140624caaa64e
falihnewage <falih@newagesmb.com>
2023-04-05 10:50:47 +0530

    windfow .location

--- src/views/Pages/LoginPage.js
+++ src/views/Pages/LoginPage.js
@@ -24,7 +24,7 @@ const LoginPage = () => {
   const { register, handleSubmit, formState: { errors }, reset } = useForm()
 
   const SsoLogin = () => {
-    window.open('https://hiltondevapi.newagesme.com/auth/saml/login')
+    window.location='https://hiltondevapi.newagesme.com/auth/saml/login'
     
   }
 
